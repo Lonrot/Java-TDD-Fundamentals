dfb2614685fcd4d13dc83e54607a79dafcd1f36c
Lonrot <angelschmal.1a@gmail.com>
2024-04-30 11:22:19 +0100

    Feature: CompassTest and Compass class method created

---
+++ src/main/java/Compass.java
@@ -0,0 +1,25 @@
+
+public class Compass {
+    public enum Point{
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
+
+    public enum Direction{
+        LEFT,
+        RIGHT
+    }
+
+    private Point point;
+
+    public static Point rotate(Point point, Direction direction){
+        Point finalPoint;
+        String[] northRight = {"NORTH", "EAST", "SOUTH", "WEST"};
+        String[] northLeft = {"NORTH", "EAST", "SOUTH", "WEST"};
+
+
+        return Point.NORTH;
+    }
+}
