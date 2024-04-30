c9e62dee011f4c67ceb0b64cbedb92bb53c8c4a2
Lonrot <angelschmal.1a@gmail.com>
2024-04-30 15:05:15 +0100

    Fea:reverseString() completed and test done and pass

--- src/main/java/StringManipulator.java
+++ src/main/java/StringManipulator.java
@@ -5,12 +5,19 @@ public class StringManipulator {
 
     public String reverseString(String input) {
         // TODO: Implement the logic to reverse the input string
+
         return StringUtils.reverse(input);
     }
 
     public boolean isPalindrome(String input) {
         // TODO: Implement the logic to check if the input string is a palindrome
         // (A palindrome = same forwards as backwards)
-        return false;
+        for (int i = 0; i<input.length()/2;i++){
+            if(input.charAt(i) != input.charAt(input.length()-i-1)){
+                return false;
+            }
+
+        }
+        return true;
     }
 }
